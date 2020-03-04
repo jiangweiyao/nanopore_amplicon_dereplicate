f6d1c9a9ce9a7fc3e4bac311b8bd53b99ca3e610
Your Name <yao.jiangwei@gmail.com>
2020-03-04 14:29:09 -0500

    5th commit

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -1,9 +1,9 @@
 cd ~
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh -b
-echo 'export PATH=~/miniconda3/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
+echo "export PATH=~/miniconda3/bin:$PATH" >> ~/.bashrc
+. ~/.bashrc
 conda init bash
-source ~/.bashrc
+. ~/.bashrc
 conda config --set auto_activate_base false
 rm Miniconda3-latest-Linux-x86_64.sh
